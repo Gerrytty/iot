2c60837e2e176fceec619cd94624043e71b24f5f
Mihaylova Yuliya <mihaylova.yuliyaa@gmail.com>
2022-04-20 19:56:50 +0300

    change sensor updater

--- project/consumer.py
+++ project/consumer.py
@@ -6,6 +6,7 @@ from threading import Thread
 class SensorsUpdater:
     def __init__(self, topic_name, bootstrap_servers):
         self.arr_of_vals_of_sensor = []
+        self.arr_of_vals_of_sensor_humidity = []
         self.consumer = KafkaConsumer(topic_name, bootstrap_servers=bootstrap_servers)
         self.stop = False
 
@@ -15,8 +16,10 @@ class SensorsUpdater:
                 print(message)
                 msg = json.loads(message.value.decode("utf-8"))
                 for sensor in msg:
-                    if sensor["sensorID"] == 1:
+                    if sensor["sensorID"] == 0:
                         self.arr_of_vals_of_sensor.append(sensor["value"])
+                    elif sensor["sensorID"] == 1:
+                        self.arr_of_vals_of_sensor_humidity.append(sensor["value"])
 
     def endless_consume(self):
         new_thread = Thread(target=self.consume_and_update)
